9cc1116030f36bb9db96ee4ca0e7c34dfde0226f
José Antonio Córdoba Gómez <joseinn@correo.ugr.es>
2021-05-07 12:53:09 +0200

    processing openweather format to mine: date;temp;hum

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import json
 import os
 import time
 import requests
+from datetime import datetime
 
 
 import middleware
@@ -13,7 +14,7 @@ city        = "granada"
 language    = "ES"
 lat = "37.18817"
 lon = "-3.60667"
-url = "https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s&lang=%s" % (city,api_key,language)
+url = "https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&appid=%s&units=metric" % (lat,lon, api_key)
 # "https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=37.18817&lon=-3.60667&dt=3&appid=6dce3d8c333806792bdc5074fd53d1ea"
 
 
@@ -30,23 +31,41 @@ def test():
 @app.route("/servicio/v3/prediccion/24horas",methods=['GET'])
 def hours_24():
     response = requests.get(url)
-    response = Response(json.dumps(json.loads(response.text)), status=200)
-    #response = Response(json.dumps(predict_weather(24)), status=200)
+    data = json.loads(response.text)
+    prediction = [{
+            'date': datetime.utcfromtimestamp(d['dt']).strftime('%Y-%m-%d %H:%M:%S'),
+            'temp': d['temp'],
+            'hum': d['humidity']
+        } for d in data['hourly']]
+
+    response = Response(json.dumps(prediction[0:24]), status=200)
     return response
 
 
 @app.route("/servicio/v3/prediccion/48horas",methods=['GET'])
 def hours_48():
     response = requests.get(url)
-    response = Response(json.dumps(json.loads(response.text)), status=200)
-    #response = Response(json.dumps(predict_weather(48)), status=200)
+    data = json.loads(response.text)
+    prediction = [{
+            'date': datetime.utcfromtimestamp(d['dt']).strftime('%Y-%m-%d %H:%M:%S'),
+            'temp': d['temp'],
+            'hum': d['humidity']
+        } for d in data['hourly']]
+
+    response = Response(json.dumps(prediction[0:48]), status=200)
     return response
 
 @app.route("/servicio/v3/prediccion/72horas",methods=['GET'])
 def hours_72():
     response = requests.get(url)
-    response = Response(json.dumps(json.loads(response.text)), status=200)
-    #response = Response(json.dumps(predict_weather(72)), status=200)
+    data = json.loads(response.text)
+    prediction = [{
+            'date': datetime.utcfromtimestamp(d['dt']).strftime('%Y-%m-%d %H:%M:%S'),
+            'temp': d['temp'],
+            'hum': d['humidity']
+        } for d in data['hourly']]
+
+    response = Response(json.dumps(prediction[0:72]), status=200)
     return response
 
 @app.after_request
